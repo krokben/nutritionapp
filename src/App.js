f0dd6c83e884336eefcfeab3065fd590567848bd
Tommy Svensson <tommykajsvensson@gmail.com>
2017-11-25 16:35:36 +0100

    setup sass

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import BEMHelper from 'react-bem-helper';
+import './App.css';
 import rdiActions from './actions/rdiActions';
 import Header from './components/Header';
 import RdiSettings from './components/RdiSettings';
 
+const bem = new BEMHelper({name: 'app'});
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div {...bem()}>
         <Header />
         <RdiSettings />
       </div>
